b3a355db57b1465fc8875f70c1e3667639e65ae1
Daniel Brendel <dbrendel1988@gmail.com>
2022-10-23 21:38:09 +0200

    Scroll to top

--- resources/views/layouts/layout_view.blade.php
+++ resources/views/layouts/layout_view.blade.php
@@ -155,6 +155,12 @@
             @include('widgets.about')
             @include('widgets.footer')
             @include('widgets.login')
+
+            <div class="scroll-to-top">
+		        <div class="scroll-to-top-inner">
+			        <a href="javascript:void(0);" onclick="window.scroll({top: 0, left: 0, behavior: 'smooth'});"><i class="fas fa-arrow-up fa-2x up-color"></i></a>
+		        </div>
+	        </div>
         </main>
 
         <script src="{{ asset('js/app.js') }}"></script>
